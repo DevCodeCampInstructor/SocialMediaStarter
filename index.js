2bb8c295d9804c8fc7aa7f23592b882fe0c541af
JJ <jj.vega86@gmail.com>
2021-12-01 12:33:39 -0500

    Ported over project from different repository, modified .gitignore to hide config details

---
+++ index.js
@@ -0,0 +1,29 @@
+const connectDb = require("./db/db");
+const usersRouter = require("./routes/users");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+connectDb();
+
+//TODO: Test all routes in Postman. Export collection and include in repository with instructions on how to import and use with API
+/**
+ * POST register TESTED SUCCESS
+ * POST login NOT TESTED
+ * GET all users NOT TESTED
+ * DELETE a user NOT TESTED
+ */
+//TODO: Add README with detailed instructions on how to set up and use API
+//TODO: Add TODO comments and informational comments throughout to clarify concepts
+//TODO: Add MongoDb VSCode extension connection for grading database
+
+//! Add instruction on installing the Better Comments extension to make instructional comments easier to interact with
+
+app.use(cors());
+app.use(express.json());
+app.use(`/api/users`, usersRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
